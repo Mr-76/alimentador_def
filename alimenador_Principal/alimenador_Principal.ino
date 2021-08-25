f7ca0acf656ad0796944a1ef84eecfcb361d2161
holand76 <thegreath76@gmail.com>
2021-08-25 08:04:05 -0300

    funçoes melhores

--- alimenador_Principal/alimenador_Principal.ino
+++ alimenador_Principal/alimenador_Principal.ino
@@ -3,12 +3,15 @@
 #include<RTClib.h>
 
 RTC_DS1307 RTC; // objeto rtc relogio 
-
 Servo myservo; // def o objeto servo se referindo ao servo motor
 
-int angulo = 0;   //inteiro para declarar posiçao
+int fechado;
+int angulo;   //inteiro para declarar posiçao
+int repeticao;
+int tempo;
 
 void setup() 	{
+  
 Serial.begin(57600);
 Wire.begin();
 RTC.begin();
@@ -23,7 +26,13 @@ myservo.attach(9); // define que pin o servo esta conectado
   
 		}
 void loop() {
-myservo.write(50);
+
+angulo = 60;
+repeticao = 10;
+tempo = 1200;
+fechado = 80;
+myservo.write(fechado);
+
 DateTime now = RTC.now();
 Serial.print(now.year(), DEC);
     Serial.print('/');
@@ -51,21 +60,21 @@ Serial.print(now.year(), DEC);
 if (now.hour() == 17 && now.minute() == 28 && now.second() == 30){
 
 //time goes up here
-repetidordeFuncao(1200,10,97);
+repetidordeFuncao(tempo,repeticao,angulo,fechado);
 								 }
 
                  
 if (now.hour() == 15 && now.minute() == 28 && now.second() == 30){
 
 //time goes up here
-repetidordeFuncao(1200,10,97);
+repetidordeFuncao(tempo,repeticao,angulo,fechado);
                 }
 
                 
 if (now.hour() == 12 && now.minute() == 28 && now.second() == 30){
 
 //time goes up here
-repetidordeFuncao(1200,10,97);
+repetidordeFuncao(tempo,repeticao,angulo,fechado);
                  }
 
 
@@ -73,24 +82,24 @@ repetidordeFuncao(1200,10,97);
 if (now.hour() == 8 && now.minute() == 28 && now.second() == 30){
 
 //time goes up here
-repetidordeFuncao(1200,10,97);
+repetidordeFuncao(tempo,repeticao,angulo,fechado);
                 }
 
 
 	    }
 
-int repetidordeFuncao(int segundos,int repeticao,int angulo){//later use pointers to call the function
+void repetidordeFuncao(int segundos,int repeticao,int angulo, int fechando){//later use pointers to call the function
 //funcao abre e fecha abertura do cano segundos se refere ao tempo aberto em mili secs , repeticao se refere ao numero de vezes 
 //para abrir e fechar a abertura angulo se refere ao angulo de abertura.
 int i = 0;
 for(i;i<repeticao;i++)	{
-AbrindoeFechando(segundos,angulo);
+AbrindoeFechando(segundos,angulo,fechado);
 			}
 						}
 
-int AbrindoeFechando(int segundos,int angulo){
+void AbrindoeFechando(int segundos,int angulo,int fechado){
 myservo.write(angulo);
 delay(segundos);
-myservo.write(50); //fecha //closes
+myservo.write(fechado); //fecha //closes
 delay(segundos);
 				}
