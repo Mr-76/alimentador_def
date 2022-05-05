95e2bc202783454b313ceb1ec03dc4253d7ce675
Mr-76 <thegreath76@gmail.com>
2022-05-05 15:59:23 -0300

    changes folder name,new timings

--- alimentador_principal/servo_bomba_agua/servo_bomba_agua.ino
+++ alimentador_principal/servo_bomba_agua/servo_bomba_agua.ino
@@ -1,6 +1,6 @@
-#include <Servo.h>
-#include<Wire.h>
-#include "RTClib.h"
+#include "Servo.h"
+#include <Wire.h>
+#include <RTClib.h>
 #include <Keypad.h>
 
 RTC_DS1307 RTC; // objeto rtc relogio 
@@ -8,6 +8,7 @@ Servo myservo; // def o objeto servo se referindo ao servo motor
 int PINO_RELE = 3;
 
 void setup(){
+  pinMode(LED_BUILTIN, OUTPUT);
 	pinMode(PINO_RELE,OUTPUT);
 	Serial.begin(57600);
 	RTC.begin();
@@ -20,6 +21,8 @@ void setup(){
 	myservo.attach(2); // define que pin o servo esta conectado 
 }
 
+DateTime now = RTC.now(); //ser visto no escopo global colocar nas func depois
+
 void loop() {
 	DateTime now = RTC.now();
 	Serial.print(now.hour(), DEC);
@@ -30,16 +33,17 @@ void loop() {
 	Serial.println();
 	delay(1000);
 	
-	ativado(5,12,15,5);
-	watering_plants(6,30,360000);
+	ativado(5,12,18,5);
+	watering_plants(6,30,3);
 
 }
 
 void watering_plants(int hour,int minute,int tempo_aguar){
 	//liga o rele da bomba de agua
+	//tempo de aguar em minutos
 	if (now.hour() == hour && now.minute() == minute){
 		digitalWrite(PINO_RELE,HIGH);
-		delay(tempo_aguar);
+		delay(tempo_aguar*60000);
 		digitalWrite(PINO_RELE,LOW);
   	}
 }
@@ -49,10 +53,10 @@ void repetidordeFuncao(int segundos,int repeticao,int angulo, int fechando){
 	//aberto em mili secs , repeticao se refere ao numero de vezes 
 	//para abrir e fechar a abertura e angulo se refere ao angulo de abertura
 	for(int i = 0;i<repeticao;i++)  {
-		AbrindoeFechando(segundos,angulo,fechado);
+		AbrindoeFechando(segundos,angulo,fechando);
 	}
 	delay(70000); //delay para pular os minutos
-	myservo.write(fechado);
+	myservo.write(fechando);
 }
 
 void AbrindoeFechando(int segundos,int angulo,int fechado){
@@ -70,13 +74,16 @@ void ativado(int hora1,int hora2,int hora3,int minutos){
 	int tempo = 920;
 	int fechado = 80;
 
-	int array_horario = {5,12,16}
-	int size_array = (sizeof(array_horario)/sizeof(int));
-	for(i=0;i<size_array;i++){
-		if (now.hour()) == array_horario[i]{
-			if(now.minute()) == 5{
+	int array_horario[] = {hora1,hora2,hora3};
+	int size_array = (sizeof(array_horario)/sizeof(int));//tamanho do array para loop
+	for(int i = 0;i < size_array;i++){
+		if ((now.hour()) == (array_horario[i])){
+			if ((now.minute()) == (minutos)){
 				repetidordeFuncao(tempo,repeticao,angulo,fechado);
 			}
+		}
+		else{
+			continue;
 		}
 	}
 }
